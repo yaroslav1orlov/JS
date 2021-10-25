ad5bd9378dca7e7e0500c8ddfb8e4ce4f7798823
Yaroslav Orlov <yaroslav1orlov@gmail.com>
2021-10-25 11:20:58 +0300

    Added loop

--- firstProj/main.js
+++ firstProj/main.js
@@ -1,8 +1,14 @@
-console.log('. .');
-console.log(' - ');
-// ---
+console.log('=^.^=');
+var drawCats = function (hawManyTimes) {
+    for (var i = 0; i < hawManyTimes; i++){
+        console.log(i + " =^.^= ");
+    }
+};
+drawCats(10);
+ 
+// -----------
 console.log('Yaroslav')
-// ---
+// ---------------------
 // Boom!
 var a = 2;
 var b = 3;
