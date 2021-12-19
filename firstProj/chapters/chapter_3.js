8f346a6c1c1dbf6f425ab5e8e282111a5d1fb1bb
Yaroslav Orlov <yaroslav1orlov@gmail.com>
2021-12-19 19:10:39 +0200

    Added chapter stucture

--- firstProj/chapters/chapter_3.js
+++ firstProj/chapters/chapter_3.js
@@ -6,3 +6,12 @@ for (let i = 0; i < dinosaurs.length; i++) {
     console.log(element);
 }
 //_______________________________________________
+var dinosaurs = [
+"T-rex", "Velccirator", "Stegosaurus", "Triceratops", "Brachiosaurus",
+"Pteranodon", "Apatosaurus", "Diplodocus", "Compsognathus"
+];
+
+for (let i = 0; i < dinosaurs.length; i++) {
+    const element = dinosaurs[i];
+    console.log(element);
+}
