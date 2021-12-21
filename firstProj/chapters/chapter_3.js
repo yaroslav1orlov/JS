56c1383e17e5bef25f1688a38379e04c778e055f
Yaroslav Orlov <yaroslav1orlov@gmail.com>
2021-12-21 19:54:04 +0200

    Added one more way to initialize an array

--- firstProj/chapters/chapter_3.js
+++ firstProj/chapters/chapter_3.js
@@ -1,12 +1,14 @@
 var dinosaurs = ["T-rex", "Velccirator", "Stegosaurus", "Triceratops", "Brachiosaurus",
                 "Pteranodon", "Apatosaurus", "Diplodocus", "Compsognathus"];
 
-for (let i = 0; i < dinosaurs.length; i++) {
-    const element = dinosaurs[i];
-    console.log(element);
+var print = function(arr){
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        console.log(element);
+    }
 }
 //_______________________________________________
-var dinosaurs = [
+var din1 = [
 "T-rex",
  "Velccirator",
   "Stegosaurus",
@@ -18,13 +20,23 @@ var dinosaurs = [
         "Compsognathus"
 ];
 
-for (let i = 0; i < dinosaurs.length; i++) {
-    const element = dinosaurs[i];
-    console.log(element);
-}
+print(din1)
 // ______________________________________________
 var dinosaur1 = "T-rex";
 console.log(dinosaur1);
 var dinosaur3 = "Stegosaurus";
 console.log(dinosaur3);
+//_______________________________________________
+var din2 = []
+din2[1] = "T-rex";
+din2[2] = "Velccirator";
+din2[3] = "Stegosaurus";
+din2[4] = "Triceratops";
+din2[5] = "Brachiosaurus";
+din2[6] = "Pteranodon";
+din2[7] = "Apatosaurus";
+din2[8] = "Diplodocus";
+din2[9] = "Compsognathus";
+
+print(din2)
 //_______________________________________________
