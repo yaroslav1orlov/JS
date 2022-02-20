cc29ac30eef69b9b69bb02a1c37a653137257dd9
Yaroslav Orlov <yaroslav1orlov@gmail.com>
2022-02-20 15:09:25 +0200

    bug from line 72 to line 80. where is the bug? No bug, fixed it.

--- firstProj/chapters/chapter_4.js
+++ firstProj/chapters/chapter_4.js
@@ -31,7 +31,7 @@ console.log(cat.nose);
 console.log(cat.body);
 //_______________________________________________
 var kitten = {};
-    kitten['legs'] = 4,
+kitten['legs'] = 4,
     kitten['paws'] = 4,
     kitten["eyes"] = 2,
     kitten['ears'] = 2,
@@ -40,45 +40,48 @@ var kitten = {};
     kitten['head'] = 1,
     kitten['nose'] = 1,
     kitten['body'] = 1
-    console.log(kitten);
-    //способ 1
-    console.log(kitten['legs']);
-    console.log(kitten['paws']);
-    console.log(kitten['eyes']);
-    console.log(kitten['ears']);
-    console.log(kitten['tail']);
-    console.log(kitten['mouse']);
-    console.log(kitten['head']);
-    console.log(kitten['nose']);
-    console.log(kitten['body']);
-    //способ 2
-    console.log(kitten.legs);
-    console.log(kitten.paws);
-    console.log(kitten.eyes);
-    console.log(kitten.ears);
-    console.log(kitten.tail);
-    console.log(kitten.mouse);
-    console.log(kitten.head);
-    console.log(kitten.nose);
-    console.log(kitten.body);
+console.log(kitten);
+//способ 1
+console.log(kitten['legs']);
+console.log(kitten['paws']);
+console.log(kitten['eyes']);
+console.log(kitten['ears']);
+console.log(kitten['tail']);
+console.log(kitten['mouse']);
+console.log(kitten['head']);
+console.log(kitten['nose']);
+console.log(kitten['body']);
+//способ 2
+console.log(kitten.legs);
+console.log(kitten.paws);
+console.log(kitten.eyes);
+console.log(kitten.ears);
+console.log(kitten.tail);
+console.log(kitten.mouse);
+console.log(kitten.head);
+console.log(kitten.nose);
+console.log(kitten.body);
 //_______________________________________________
 var dinosaura = [
-{ name: "TyrannosaurusRex", reriod: "UpperCretaceous" },
-{ name: "Stegosaurus", regiod: "UpperKorsky" },
-{ name: "Platheovaur", reriod: "Triassic" }
+    { name: "TyrannosaurusRex", period: "UpperCretaceous" },
+    { name: "Stegosaurus", period: "UpperKorsky" },
+    { name: "Platheovaur", period: "Triassic" }
 ];
 console.log(dinosaura);
-    //способ 1
-    console.log(dinosaura['TyrannosaurusRex']);
-    console.log(dinosaura['UpperCretaceous']);
-    console.log(dinosaura['Stegosaurus']);
-    console.log(dinosaura['UpperKorsky']);
-    console.log(dinosaura['Platheovaur']);
-    console.log(dinosaura['Triassic']);
-    //способ 2
-    console.log(dinosaura.TyrannosaurusRex);
-    console.log(dinosaura.UpperCretaceous);
-    console.log(dinosaura.Stegosaurus);
-    console.log(dinosaura.UpperKorsky);
-    console.log(dinosaura.Platheovaur);
-    console.log(dinosaura.Triassic);
+//способ 1
+console.log(dinosaura[0]);
+console.log('name: ' + dinosaura[0]['name']);
+console.log('period: ' + dinosaura[0]['period']);
+console.log(dinosaura[1]);
+console.log('name: ' + dinosaura[1]['name']);
+console.log('period: ' + dinosaura[1]['period']);
+console.log(dinosaura[2]);
+console.log('name: ' + dinosaura[2]['name']);
+console.log('period: ' + dinosaura[2]['period']);
+//способ 2
+console.log("name: " + dinosaura[0].name);
+console.log("period: " + dinosaura[0].period);
+console.log("name: " + dinosaura[1].name);
+console.log("period: " + dinosaura[1].period);
+console.log("name: " + dinosaura[2].name);
+console.log("period: " + dinosaura[2].period);
